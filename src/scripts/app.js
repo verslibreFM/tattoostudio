57327a2066762efafbb33486b61252210c1a9716
verslibremd <68556094+verslibremd@users.noreply.github.com>
2021-04-04 12:16:51 +0300

    main

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -23,18 +23,35 @@ but.forEach(function (elem){
    });
 });
 
+$(document).ready(function () {
+   $('.next').click(function (e) {
+      e.preventDefault();
+      let currentImage = $('.img.curry');
+      let currentImageIndex =  $('.img.curry').index();
+      let nextImageIndex = currentImageIndex + 1;
+      let nextImage = $('.img').eq(nextImageIndex);
+      currentImage.fadeOut(1000);
+      currentImage.removeClass('curry');
 
+      if (nextImageIndex == ($('.img:last').index()+1)){
+         $('.img').eq(0).fadeIn(1000);
+         $('.img').eq(0).addClass('curry');
+      }else{
+         nextImage.fadeIn(1000);
+         nextImage.addClass('curry');
+      }
+   });
 
+   $('.prev').click(function (e) {
+      e.preventDefault();
+      let currentImage = $('.img.curry');
+      let currentImageIndex = $('.img.curry').index();
+      let prevImageIndex = currentImageIndex - 1;
+      let prevImage = $('.img').eq(prevImageIndex);
 
-
-
-
-
-
-
-
-
-
-
-
-//mouseover
+      currentImage.fadeOut(1000);
+      currentImage.removeClass('curry');
+      prevImage.fadeIn(1000);
+      prevImage.addClass('curry');
+   });
+});
